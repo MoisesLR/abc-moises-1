e4d506ffe0f20b4d28a61c473f9f8bb44a6da8db
moiso920 <>
2021-10-13 16:19:51 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>// b.//
+
+using namespace std;// a.//
+int main() {// a.//
+cout<< "Moises \n";// Aqui puse mi unico nombre.//
+cout<< "Lopez \n";// Aqui puse mi primer apellido.//
+cout<< "Rojas \n";// Aqui puse mi segundo apellido.//
+cout<< "10-2 b";// Aqui puse mi sección.//
+}// a.//
